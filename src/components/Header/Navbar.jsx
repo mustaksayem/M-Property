7926b1b80be689cb32c8c8449e0933421b1e3e3c
Sayem <mustakhasansayem@gmail.com>
2024-04-18 18:38:18 +0600

    Slider Done

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52'>
+            className='menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-white rounded-box w-52'>
             {allLink}
           </ul>
         </div>
--- src/components/body/Slider.jsx
+++ src/components/body/Slider.jsx
@@ -20,7 +20,14 @@ const Slider = () => {
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        className='mySwiper'>
+        className='mySwiper relative'>
+          <div className="z-50 absolute top-10 text-black left-1/2 transform -translate-x-1/2">
+            <p className="font-bold text-4xl mb-5">Search properties for sale or rent</p>
+            <p><label className="input input-bordered flex items-center gap-2">
+  <input type="text" className="grow" placeholder="Search" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+</label></p>
+          </div>
         <SwiperSlide>
           <img
             className='lg:h-[calc(100vh-130px)] w-auto lg:w-full rounded-2xl '
@@ -38,7 +45,7 @@ const Slider = () => {
         <SwiperSlide>
           <img
             className='lg:h-[calc(100vh-130px)] w-auto lg:w-full rounded-2xl'
-            src='https://i.ibb.co/fvCqCq1/4.jpg'
+            src='https://i.ibb.co/bJmLn2r/sslide-03.jpg'
             alt='Slider-3'
           />
          
